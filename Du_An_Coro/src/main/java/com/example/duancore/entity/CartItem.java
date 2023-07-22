4e13bdc4d41f1d9b5c113661195099d54899a41d
vietnb25 <nhunnhun2503@gmail.com>
2023-07-22 09:48:32 +0700

    carrtt

---
+++ Du_An_Coro/src/main/java/com/example/duancore/entity/CartItem.java
@@ -0,0 +1,11 @@
+package com.example.duancore.entity;
+
+import jakarta.persistence.Id;
+
+public class CartItem {
+    @Id
+    private String ma;
+
+
+    // Constructors, getters, setters
+}
---
+++ Du_An_Coro/src/main/java/com/example/duancore/service/ShoppingCartService.java
@@ -0,0 +1,5 @@
+package com.example.duancore.service;
+
+public interface ShoppingCartService {
+
+}
