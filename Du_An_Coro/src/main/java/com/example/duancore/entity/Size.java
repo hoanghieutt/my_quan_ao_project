6a604d05a8135f7d7bf273144edf1962e8088fdd
vietnb25 <nhunnhun2503@gmail.com>
2023-07-22 09:49:10 +0700

    k co j

--- Du_An_Coro/src/main/java/com/example/duancore/entity/Size.java
+++ Du_An_Coro/src/main/java/com/example/duancore/entity/Size.java
@@ -1,19 +1,30 @@
 package com.example.duancore.entity;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-public class Size implements Serializable {
+@Table(name = "Size")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Size {
 
      @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "maSize")
      private String maSize;
 
      private String size;
@@ -22,63 +33,5 @@ public class Size implements Serializable {
      private Date ngaySua;
      private int trangThai;
 
-    public Size() {
-    }
 
-    public Size(String maSize, String size, String soLuong, Date ngayTao, Date ngaySua, int trangThai) {
-        this.maSize = maSize;
-        this.size = size;
-        this.soLuong = soLuong;
-        this.ngayTao = ngayTao;
-        this.ngaySua = ngaySua;
-        this.trangThai = trangThai;
-    }
-
-    public String getMaSize() {
-        return maSize;
-    }
-
-    public void setMaSize(String maSize) {
-        this.maSize = maSize;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getSoLuong() {
-        return soLuong;
-    }
-
-    public void setSoLuong(String soLuong) {
-        this.soLuong = soLuong;
-    }
-
-    public Date getNgayTao() {
-        return ngayTao;
-    }
-
-    public void setNgayTao(Date ngayTao) {
-        this.ngayTao = ngayTao;
-    }
-
-    public Date getNgaySua() {
-        return ngaySua;
-    }
-
-    public void setNgaySua(Date ngaySua) {
-        this.ngaySua = ngaySua;
-    }
-
-    public int getTrangThai() {
-        return trangThai;
-    }
-
-    public void setTrangThai(int trangThai) {
-        this.trangThai = trangThai;
-    }
 }
--- Du_An_Coro/src/main/java/com/example/duancore/repository/MauSacRepository.java
+++ Du_An_Coro/src/main/java/com/example/duancore/repository/MauSacRepository.java
@@ -1,7 +1,9 @@
 package com.example.duancore.repository;
 
+import com.example.duancore.entity.MauSac;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MauSacRepository extends Repository{
+public interface MauSacRepository extends JpaRepository<MauSac, String> {
 }
